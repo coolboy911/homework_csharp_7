52391969dfe5e9e889fc6d2a47232d039bd97d54
Ilya Plutov <ilya.plutov@gmail.com>
2023-02-06 00:42:38 +0300

    fixed task47

--- task47/Program.cs
+++ task47/Program.cs
@@ -26,7 +26,7 @@ void PrintArray(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-             
+            Console.Write($"{array[i, j]:f3}\t");
         }
         Console.WriteLine();
     }
